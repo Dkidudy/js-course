2f2278f84fff647fe5e9a54cc45dd7743fa9960f
Dkidudy <izripov2006@gmail.com>
2026-01-14 13:15:06 +0300

    add index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let title = "dev";
+let screens = [];
+let screenPrice = 0;
+let rollback = 0;
+let fullPrice = 0;
+let adaptive = false;
+
+alert("Скрипт подключён и работает.");
+
+console.log("Сообщение из консоли: ошибок быть не должно.");
+
+console.log({ title, screens, screenPrice, rollback, fullPrice, adaptive });

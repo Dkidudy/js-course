5ee8b4bf97577139126640bac7d26701d4295d53
Dkidudy <izripov2006@gmail.com>
2026-01-26 18:00:20 +0300

    objects,arrays,pseudo-arrays

--- script.js
+++ script.js
@@ -1,21 +1,89 @@
 "use strict";
 
-const game = () => {
-  const secret = Math.floor(Math.random() * 100) + 1; // округляет числа в большую сторону
+const appData = {
+  title: "",
+  screens: "",
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 10,
+  allServicePrices: 0,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  service1: "",
+  service2: "",
+  asking: function () {
+    appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+    appData.screens = prompt(
+      "Какие типы экранов нужно разработать?",
+      "Простые, Сложные",
+    );
 
-  const ask = () => {
-    const input = prompt("Угадай число от 1 до 100");
+    do {
+      appData.screenPrice = prompt("Сколько будет стоит данная работа?");
+    } while (!appData.isNumber(appData.screenPrice));
 
-    const num = Number(input);
-    if (!Number.isFinite(num)) return (alert("Введи число!"), ask()); // кидает ошибку если я ввел буквы
-    if (num > secret) return (alert("Загаданное число меньше"), ask());
-    if (num < secret) return (alert("Загаданное число больше"), ask());
-    if (input === null) return alert("Игра окончена");
+    appData.adaptive = prompt("Нужен ли адаптив на сайте?", "Да");
+  },
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+  getAllServicePrices: function () {
+    let sum = 0;
+    for (let i = 0; i < 2; i++) {
+      if (i === 0) {
+        appData.service1 = prompt("Какой дополнительный тип услуги нужен?");
+      } else if (i === 1) {
+        appData.service2 = prompt("Какой дополнительный тип услуги нужен?");
+      }
 
-    alert("Поздравляю, Вы угадали!");
-  };
-
-  ask();
+      sum += +prompt("Сколько это будет стоить?");
+    }
+    return sum;
+  },
+  getFullPrice: function () {
+    return appData.screenPrice + appData.allServicePrices;
+  },
+  getServicePercentPrices: function () {
+    return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+  },
+  getTitle: function () {
+    return (
+      appData.title.trim()[0].toUpperCase() +
+      appData.title.trim().substr(1).toLowerCase()
+    );
+  },
+  getRollbackMessage: function (price) {
+    if (price >= 30000) {
+      return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 3000) {
+      return "Даем скидку в 5%";
+    } else if (price >= 0 && price < 15000) {
+      return "Скидка не предусмотрена";
+    } else {
+      return "Что то пошло не так";
+    }
+  },
+  start: function () {
+    appData.asking();
+    appData.allServicePrices = appData.getAllServicePrices();
+    appData.fullPrice = appData.getFullPrice();
+    appData.servicePercentPrice = appData.getServicePercentPrices();
+    appData.title = appData.getTitle();
+    appData.logger();
+  },
+  logger: function () {
+    console.log("allServicePrices", appData.allServicePrices);
+    console.log(appData.getRollbackMessage(appData.fullPrice));
+    console.log(appData.title);
+    console.log(appData.screenPrice);
+    console.log(appData.adaptive);
+    console.log(appData.screens.length);
+    console.log(appData.servicePercentPrice);
+    for (const key in this) {
+      if (Object.prototype.hasOwnProperty.call(this, key)) {
+        console.log(`${key}, this[key]`);
+      }
+    }
+  },
 };
-
-game();
+appData.start();

e1f3762f7fdbb3f1ce48c80e12e718a98a7f0851
Dkidudy <izripov2006@gmail.com>
2026-01-29 12:44:11 +0300

    Add lesson10

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-const title = "js сourse";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 500; // любое число
-const rollback = 50; // откат
-const fullPrice = 5500; // столько хочу заработать
-const adaptive = true; // булевое значение
+// 1 Восстановить порядок книг.
+const booksContainer = document.querySelector(".books");
+const books = booksContainer.querySelectorAll(".book");
+const book1 = books[1];
+const book2 = books[0];
+const book3 = books[4];
+const book4 = books[3];
+const book5 = books[5];
+const book6 = books[2];
+booksContainer.append(book1, book2, book3, book4, book5, book6);
 
-console.log(typeof title); // string название
-console.log(typeof fullPrice); // number
-console.log(typeof adaptive);
+// 2 Заменить картинку заднего фона на другую из папки image
+document.head.insertAdjacentHTML(
+  "beforeend",
+  '<style>body{background-image:url("./image/you-dont-know-js.jpg")}</style>',
+);
 
-// Стоимость верстки экранов и стоимостб разработки сайта
-console.log(`Стоимость верстки экранов ${screenPrice} $`);
-console.log(`Стоимость разработки сайта ${fullPrice} $`);
+// 3 Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+book3.querySelector("a").textContent = "Книга 3. this и Прототипы Объектов";
 
-const screensArray = screens
-  .toLowerCase()
-  .split(",")
-  .map((item) => item.trim());
-console.log(screensArray);
+// 4 Удалить рекламу со страницы
+const adv = document.querySelector(".adv");
+adv.remove();
 
-const rollbackAmount = fullPrice * (rollback / 1100);
-console.log(`Процент отката посреднику за работу: ${rollbackAmount}`);
+// 5 Восстановить порядок глав во второй и пятой книге
+
+// Книга 2
+const book2List = book2.querySelector("ul");
+const book2Items = book2List.querySelectorAll("li");
+// Книга 5
+const book5List = book5.querySelector("ul");
+const book5Items = book5List.querySelectorAll("li");
+book5List.append(
+  book5Items[0],
+  book5Items[1],
+  book5Items[9],
+  book5Items[3],
+  book5Items[4],
+  book5Items[2],
+  book5Items[6],
+  book5Items[7],
+  book5Items[5],
+  book5Items[8],
+  book5Items[10],
+);
+
+// 6. в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+const book6List = book6.querySelector("ul");
+const book6Items = book6List.querySelectorAll("li");
+
+const chapter8 = document.createElement("li");
+chapter8.textContent = "Глава 8: За пределами ES6";
